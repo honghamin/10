fb454960234a428d8b98d10a40cd2859cae99b82
honghamin <113421639+honghamin@users.noreply.github.com>
2022-11-15 17:13:30 +0900

    10-07

--- main.c
+++ main.c
@@ -3,18 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap (int *x, int *y)
-{
-	int temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-	}
-	
-int main(int argc, char *argv[]) {
-	
-		int a = 3;
-		int b = 5;
-		swap(&a, &b);
-		printf("a:%i, b:%i\n", a,b);
-	}
+
+  char*proverb = "All thath gliters is noy gold.";
+  void setPointer(char*q)
+  {
+  	q = proverb;
+  }
+  int main (void)
+  {
+  	char*p = "zzz";
+  	setPointer(p);
+	  printf("%s\n", p);
+	  
+	  return 0;
+  }
+
