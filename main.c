94fe3280eb9c52033fec04764220194deea087e4
honghamin <113421639+honghamin@users.noreply.github.com>
2022-11-15 17:38:27 +0900

    10-07 수정

--- main.c
+++ main.c
@@ -3,18 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main (int argc, char *argv[]){
-   int i = 100;
-   int*p=&i;
-   int**q = &p;
-   
-   *p = 200;
-   printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
-   
-   **q = 300;
-   printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
-   
-   return 0;
 
-  }
-
+  char*proverb = "All thath gliters is noy gold.";
+  void setPointer(char**q);
+  
+  int main (int argc, char *argv[]) {
+  	char*p = "zzz";
+  	
+  	setPointer(&p);
+  	
+  	printf("%s\n", p);
+  	
+  	return 0;
+	  }
+ void setPointer(char **q){
+ 	*q = proverb;
+ }
